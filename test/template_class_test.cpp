ec4655ba933e1629eed47527f1ab4a44149719c6
Dmytro Nikulin <dimanikulin@gmail.com>
2025-01-28 20:57:08 +0200

    #4 added simple test

--- test/template_class_test.cpp
+++ test/template_class_test.cpp
@@ -1,1 +1,16 @@
-1
+#include "template_class.h"
+#include <gtest/gtest.h>
+
+TEST(TemplateClassTest, FunctionReturnsInput)
+{
+    TemplateClass obj;
+    EXPECT_EQ(obj.function(5), 5);
+    EXPECT_EQ(obj.function(-3), -3);
+    EXPECT_EQ(obj.function(0), 0);
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
